ca11b717b6f227a3196d97de484d0342b7904a7b
DanielKolodziej <dkolodz1@hawk.iit.edu>
2020-03-09 12:47:25 -0500

    class styles to svg and useeffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,202 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
+const useStyles = makeStyles({
+  on: {
+    color: 'black',
+    backgroundColor: '#ffc324',
+    padding: '0.75em',
+    width: '6em',
+    float: 'right'
+  },
+  off: {
+    color: 'red',
+    backgroundColor: 'black',
+    padding: '0.75em',
+    width: '6em',
+    float: 'right'
+  },
+  showSVG:{
+    position: 'absolute', 
+    right: 60,
+    display: 'block',
+  },
+  noSVG:{
+    // position: 'absolute', 
+    // right: 60,
+    display: 'none',
+  }
+
+});
+
+const App = () => {
+  const classes = useStyles();
+  const [first, setFirst] = useState();
+  const [pair, setPair] = useState([]);
+  let dataInit = [
+    {
+      name: "one",
+      activated: false
+    },
+    {
+      name: "two",
+      activated: false
+    },
+    {
+      name: "three",
+      activated: false
+    },
+    {
+      name: "four",
+      activated: false
+    },
+    {
+      name: "five",
+      activated: false
+    },
+    {
+      name: "six",
+      activated: false
+    },
+  ];
+  const [data, setData] = useState(dataInit)
+  const handleDragStart = (e) => {
+    e.persist()
+    // if (pair.length >= 2){
+    //   setPair([])
+    // }
+    // if (pair.indexOf(e.target.id) === -1){
+    //   console.log('setting val')
+    //   setPair([...pair, e.target.id])
+    // }
+    // console.log(e.target.id)
+
+    // setPair([...pair, e.target.id])
+    // if (data[e.target.id].activated === false){
+    setFirst(e.target.id)
+    // }
+    // for( let i = 0; i < pair.length; i++){
+      // if (pair[i].indexOf(e.target.id) === -1){
+      //   console.log('setting val')
+      //   setPair([[...pair, e.target.id]])
+      // }
+      // setPair([[...pair, e.target.id]])
+    // }
+  }
+
+  const handleDrop = (e) => {
+    // if (pair.indexOf(e.target.id) === -1){
+    //   console.log('setting val')
+    //   setPair([...pair, e.target.id])
+    //   let temp = [...data];
+    //   temp[pair[0]].activated = true
+    //   temp[e.target.id].activated = true
+    //   setData(temp)
+    // }
+    // console.log(first)
+    // console.log(e.target.id)
+    if (e.target.id !== first){
+      let inputPair = [first, e.target.id].sort()
+      setPair([...pair, inputPair])
+      // console.log(data[inputPair[0]].activated)
+      // console.log(data[inputPair[1]].activated)
+      let temp = [...data]
+      temp[inputPair[0]].activated = true;
+      temp[inputPair[1]].activated = true;
+      // console.log(temp)
+      setData(temp)
+    }
+    
+    // for (let i= 0; i < pair.length; i++){
+    //   if (pair[i].indexOf(e.target.id) === -1){
+    //     console.log('setting val')
+    //     setPair([...pair, e.target.id])
+    //     let temp = [...data];
+    //     temp[pair[0]].activated = true
+    //     temp[e.target.id].activated = true
+    //     setData(temp)
+    //   }
+    // }
+  }
+  const handleClick = (e) => {
+      // let temp = [...data];
+      // temp[e.target.id].activated = false
+      // setData(temp)
+      let tempData = [...data]
+      let tempPair = [...pair]
+      let remove= [];
+      for(let i = 0; i < tempPair.length; i++){
+        if(tempPair[i].indexOf(e.target.id) !== -1){
+          console.log(`removing index ${i} from arr which contains ${e.target.id}`)
+          remove = tempPair.splice(i, 1)
+          // setPair(temp)
+        }
+      }
+      setPair(tempPair)
+      console.log('indexs to remove from pair and turn false: ',remove)
+      // remove[0].forEach(item =>console.log(item))
+      remove[0].forEach(item => tempData[item].activated = false)
+      setData(tempData)
+  }
+  // let svgList = document.querySelectorAll("svg[id^=svg]")
+  useEffect(() => {
+    console.log('pair',pair)
+    let svgList = document.querySelectorAll("svg[id^=svg]")
+    for (let i = 0; i < pair.length; i++){
+      for (let j = 0; j < svgList.length; j++){
+        // console.log(svgList[j].id.substring(svgList[j].id.length - 1))
+        if(pair[i][0] === svgList[j].id.substring(svgList[j].id.length - 1)){
+          console.log('match, turning display on')
+          // console.log(svgList[j].classList);
+          svgList[j].classList = 'makeStyles-showSVG-3'
+          // console.log(svgList[j].classList);
+          // makeStyles-showSVG-3
+          // svgList[j].className += 'classes.showSVG';
+
+        } else {
+          console.log('turning display off')
+          // console.log(svgList[j].classList);
+          svgList[j].classList = 'makeStyles-noSVG-4'
+          // console.log(svgList[j].classList);
+          // makeStyles-noSVG-4
+          // svgList[j].className -= 'classes.showSVG';
+        }
+      }
+    }
+  }, [pair])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((_itm, index) => (
+        <div style={{backgroundColor: '#d3d3d3', border: '1px solid black', padding: '1.5em'}}>
+        <div 
+          key={index} 
+          style={{display: 'inline'}}
+          >
+          {_itm.name}
+        </div>
+        <div 
+          name='drag'
+          draggable={!_itm.activated}
+          // {_itm.activated ? draggable : null}
+          // onDrag= {handleDrag}
+          onDragStart= {handleDragStart}
+          onDragOver= {(e) => e.preventDefault()}
+          onDrop= {handleDrop}
+          onClick={_itm.activated ? handleClick : null}
+          id={index.toString()} 
+          className={_itm.activated ? classes.on : classes.off}
+          key={index+'bool'}>
+          {_itm.activated.toString()}
+        </div>
+        {/* key={index+'svg'} */}
+        {/* className={classes.showSVG} */}
+        <svg className={classes.noSVG} id={'svg'+index} width="30" height="70"><line style={{strokeWidth:30}} x1="1" y1="1" x2="1" y2="70" stroke="blue"/></svg>
+        </div>
+      ))}
+      <div style={{backgroundColor: 'red', fontSize:'2em'}}>{pair}</div>
     </div>
   );
-}
+};
+
 
 export default App;
